7ea1cb081f965d210627d67f785dc9ce52c26f6f
Matthew Johnson (MSRC) <matjoh@microsoft.com>
2018-12-19 16:01:26 +0000

    Updated day 17 still in progress

--- day17.py
+++ day17.py
@@ -12,29 +12,41 @@ WATER = ord('~')
 SPRING = ord('+')
 WET_SAND = ord('|')
 
-Point = namedtuple('Point', ('row', 'col'))
+class Point:
+    def __init__(self, row, col):
+        self._row = row
+        self._col = col
+        self._hash = (row, col).__hash__()
+    
+    def __eq__(self, other):
+        return !(self.row != other.row or self.col != other.col)
+    
+    def __hash__(self):
+        return self._hash
+    
+    @property
+    def row(self):
+        return self._row
+
+    @property
+    def col(self):
+        return self._col
 
 DOWN = Point(1, 0)
 LEFT = Point(0, -1)
 RIGHT = Point(0, 1)
 
-class Drop(namedtuple('Drop', ('row', 'col'))):
-    def __init__(self, row, col, flowing, water):
+
+class Drop(Point):
+    def __init__(self, row, col):
         super().__init__(row, col)
-        self._flowing = flowing
-        self._water = water
-        self._left = col
-        self._right = col
-        self._flowing.add(self)
+        self._left = None
+        self._right = None
+        self._flowed = False
     
     def __add__(self, other):
-        return Drop(self.row + other.row, self.col + other.col, self._flowing, self._water)
-    
-    def _settle(self):
-        for col in range(self._left, self._right+1):
-            self._flowing.remove(Point(self.row, col))
-            self._water.add(Point(self.row, col))
-    
+        return Drop(self.row + other.row, self.col + other.col)
+       
     @property
     def left(self):
         return self._left
@@ -42,8 +54,6 @@ class Drop(namedtuple('Drop', ('row', 'col'))):
     @left.setter
     def left(self, col):
         self._left = col
-        if self._right:
-            self._settle()
 
     @property
     def right(self):
@@ -52,44 +62,89 @@ class Drop(namedtuple('Drop', ('row', 'col'))):
     @right.setter
     def right(self, col):
         self._right = col
-        if self._left:
-            self._settle()
     
-    def drop(self, clay, water):
-        if self + DOWN in (clay | water):
-            return Flow(self.row, self.col, self._flowing, True), Flow(self.row, self.col, self._flowing, False)
+    def flow(self, clay, water, flowing):
+        flowing.add(self)
+        down = self + DOWN
+        if down in (clay | water):
+            if self._flowed:
+                if self._left and self._right:
+                    for col in range(self._left, self._right + 1):
+                        point = Point(self.row, col)
+                        flowing.remove(point)
+                        water.add(point)
 
-        return self + DOWN
+                return None
+            else:
+                self._flowed = True
+                return self, Flow(self.row, self.col, True, self), Flow(self.row, self.col, False, self)
+
+        elif down not in flowing:
+            return down
+        
+        return None
 
 
-class Flow(namedtuple('Flow', ('row', 'col'))):
-    def __init__(self, row, col, left, drop):
+class Flow(Point):
+    def __init__(self, row, col, flow_left, drop):
         super().__init__(row, col)
-        self._point = point
-        self._left = left
+        self._flow_left = flow_left
         self._drop = drop
     
-    def flow(self, clay):
-        if self._left:
-            if self + left in clay:
-                
-            self._point += LEFT
+    def __add__(self, other):
+        return Flow(self.row + other.row, self.col + other.col, self._flow_left, self._drop)
+
+    def flow(self, clay, water, flowing):
+        flowing.add(self)
+        down = self + DOWN
+        if self._flow_left:
+            left = self + LEFT
+            if left in clay:
+                self._drop.left = self.col
+                return None
+
+            if down in (clay | water):
+                return left
         else:
-            self.point += RIGHT
-        
-        if self._point + LEFT in clay:
-            self._drop.left = self._point
+            right = self + RIGHT
+            if right in clay:
+                self._drop.right = self.col
+                return None
+            
+            if down in (clay | water):
+                return right            
+
+        if down in flowing:
             return None
         
-        self._point += LEFT
-        return self._point
+        return Drop(down.row, down.col)
+
+
+def line_to_points(self, line):
+    if line.startswith('x'):
+        x=495, y=2..7
+        parts = line.split(',')
+        x = int(parts[0][2:])
+        parts = parts[1][2:].split('..')
+        y0 = int(parts[0])
+        y1 = int(parts[1])
+        for y in range(y0, y1 + 1):
+            yield y, x
     
-
-
-class Ground:
-    def __init__(self, lines):
+    if line.startswith('y'):
         pass
     
+    raise ValueError("Invalid input: " + line)
+
+        
+class Ground:
+    def __init__(self, lines):
+        self._clay = set()
+        self._water = set()
+        self._flowing = set()
+        for line in lines:
+
+
     def update(self):
         pass
 
