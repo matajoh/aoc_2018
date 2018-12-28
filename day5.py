f62c8575b2300dff0fc7a62b4eda4af49603aed8
Matthew Johnson (MSRC) <matjoh@microsoft.com>
2018-12-28 07:31:46 +0000

    Improved answer to part 5

--- day5.py
+++ day5.py
@@ -1,74 +1,119 @@
 """ Solution to day 5 of the 2018 Advent of Code """
 
-from utils import read_input
+import logging
 
-# TODO rewrite to use linked lists
+from utils import read_input, assert_equal, parse_args
 
-def reduce(polymer, reduced):
-    """ Reduce the polymer following the rules """
-    current_element = None
-    for next_element in polymer:
-        if current_element is None:
-            current_element = next_element
-        else:
-            if current_element.lower() == next_element.lower():
-                if current_element == next_element:
-                    reduced.append(current_element)
-                else:
-                    current_element = None
+START = "<start>"
+END = "<end>"
+
+
+def should_react(lhs, rhs):
+    """ Whether these two elements should react """
+
+    return lhs.lower() == rhs.lower() and lhs != rhs
+
+
+class Node:
+    """ Node in a doubly-linked list """
+
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+        self.prev = None
+
+    @staticmethod
+    def parse(text, exclude):
+        """ Parses a linked list from the input """
+        start = Node(START)
+        current = start
+        for value in text:
+            if value.lower() == exclude:
+                continue
+
+            current.next = Node(value)
+            current.next.prev = current
+            current = current.next
+
+        current.next = Node(END)
+        current.next.prev = current
+        return start
+
+    def react(self):
+        """ Reacts the list using the reaction rules """
+        current = self
+        while current.next.value != END:
+            if should_react(current.value, current.next.value):
+                current.prev.next = current.next.next
+                current.next.next.prev = current.prev
+                current = current.prev
             else:
-                reduced.append(current_element)
-                current_element = next_element
+                current = current.next
 
-    if current_element is not None:
-        reduced.append(current_element)
+    def __len__(self):
+        current = self
+        num_nodes = 0
+        while current.next.value != END:
+            current = current.next
+            num_nodes += 1
 
-    return reduced
+        return num_nodes
 
 
-def react(polymer):
+def react(polymer, exclude=None):
     """ Fully react the polymer and return the final length """
-    length = len(polymer)
-    reduced = []
-    while True:
-        reduce(polymer, reduced)
-        polymer, reduced = reduced, polymer
-        reduced.clear()
-        if len(polymer) == length:
-            break
-
-        length = len(polymer)
-
-    return length
+    polymer = Node.parse(polymer, exclude)
+    polymer.react()
+    return len(polymer)
 
 
-def part1(polymer):
-    """ Solution to part 1 """
-    print(react(list(polymer)))
+def test_day5():
+    """ Test for day 5 """
+    test_input = 'dabAcCaCBAcCcaDA'
+    actual = react(test_input)
+    expected = 10
+
+    assert_equal(actual, expected)
+
+    cases = [
+        ('a', 6),
+        ('b', 8),
+        ('c', 4),
+        ('d', 6)
+    ]
+
+    for unit, expected in cases:
+        actual = react(test_input, unit)
+        assert_equal(actual, expected)
+
+    expected = 4
+    actual = improve(test_input)
+    assert_equal(actual, expected)
 
 
-def part2(original_polymer):
-    """ Solution to part 2 """
-    unique_elements = set(original_polymer.lower())
-    min_length = len(original_polymer)
+def improve(polymer):
+    """ Find which element to remove to best improve the polymer """
+    unique_elements = set(polymer.lower())
+    min_length = len(polymer)
     for element in unique_elements:
-        polymer = list(filter(lambda unit, compare=element: unit.lower()
-                              != compare, original_polymer))
-        length = react(polymer)
+        length = react(polymer, element)
         if length < min_length:
             min_length = length
-        print(element, length)
 
-    print("min length:", min_length)
+        logging.debug("element: %d length: %d", element, length)
+
+    return min_length
 
 
 def day5():
     """ Solution to day 5 """
+    parse_args()
+
     polymer = read_input(5, no_split=True)
     print("Part 1")
-    part1(polymer)
+    print(react(polymer))
     print("Part 2")
-    part2(polymer)
+    print(improve(polymer))
 
 
 if __name__ == "__main__":
